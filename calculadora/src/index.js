af50c0f5eadad3269accc2ce4ba63997891b86a9
PedroHardeman <pedrohardeman@gmail.com>
2020-12-11 17:07:09 -0300

    feature: files for 'calculator'

---
+++ calculadora/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculadora from './calculadora';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Calculadora />, document.getElementById('root'));
