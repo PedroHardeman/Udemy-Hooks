cf2870819d991cc8ee1521b2fb92b8ea5be6327d
PedroHardeman <pedrohardeman@gmail.com>
2020-12-11 19:43:24 -0300

    feature: files for 'conversor-de-moedas'

---
+++ conversor-de-moedas/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ConversorMoedas from './conversor-moedas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<ConversorMoedas />, document.getElementById('root'));
